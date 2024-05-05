68b17ed51d87fdd35c9566f8234ae9a43ba5af40
shiz.live <steven.lucero@mediakonsulterna.se>
2024-05-06 07:32:12 +0800

    Initial Design, implemented root detection and mysql connection

--- app/src/main/java/cloud/lucero/crts/control/DbConn.java
+++ app/src/main/java/cloud/lucero/crts/control/DbConn.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import android.util.Base64;
 
-public class connection {
+public class DbConn {
     private static final String URL = Base64.decode("amRiYzpteXNxbDovLzE1MS4xMDYuMTE3LjUxOjMzMDYvdTgwOTIzMDcwOF9zaGl6X2xpdmU=", Base64.DEFAULT).toString();
     private static final String USERNAME = Base64.decode("dTgwOTIzMDcwOF9zaGl6X2xpdmU=", Base64.DEFAULT).toString();
     private static final String PASSWORD = Base64.decode("QEJjMTIzMTE=" , Base64.DEFAULT).toString();
--- app/src/main/java/cloud/lucero/crts/ui/main/MainFragment.java
+++ app/src/main/java/cloud/lucero/crts/ui/main/MainFragment.java
@@ -11,6 +11,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.scottyab.rootbeer.RootBeer;
 
 import cloud.lucero.crts.R;
 
@@ -34,6 +37,12 @@ public class MainFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
         // TODO: Use the ViewModel
+        RootBeer rootBeer = new RootBeer(getContext());
+        if (!rootBeer.isRooted()) {
+            Toast.makeText(getContext(), "The system detected that your device is not rooted. Closing the app.", Toast.LENGTH_SHORT).show();
+            // getActivity().finish(); // Temporarily disabled for testing purposes
+        }
+
     }
 
 }
