0f9835842d6a44388290ceb87aa4d7732629ae4d
shiz.live <steven.lucero@mediakonsulterna.se>
2024-05-08 16:09:36 +0800

    Giving up to use mysql, reconsidering to use Firebase for cloud storage; authentication and stuff

--- app/src/main/java/cloud/lucero/crts/control/DbConn.java
+++ app/src/main/java/cloud/lucero/crts/control/DbConn.java
@@ -12,20 +12,22 @@ public class DbConn {
 
     static {
         try {
-            data = Base64.decode("amRiYzpteXNxbDovLzE1MS4xMDYuMTE3LjUxOjMzMDYvdTgwOTIzMDcwOF9zaGl6X2RiP3VzZXI9dTgwOTIzMDcwOF9zaGl6X2xpdmUmcGFzcz1AQmMxMjMxMSZ1c2VVbmljb2RlPXRydWUmY2hhcmFjdGVyRW5jb2Rpbmc9VVRGLTg=", Base64.DEFAULT);
+            data = Base64.decode("amRiYzpteXNxbDovLzE1MS4xMDYuMTE3LjUxOjMzMDYvdTgwOTIzMDcwOF9zaGl6X2RiP3VzZXI9dTgwOTIzMDcwOF9zaGl6X2xpdmUmcGFzcz1AQmMxMjMxMSZ1c2VVbmljb2RlPXRydWUmY2hhcmFjdGVyRW5jb2Rpbmc9VVRGLTgmdXNlU1NMPXRydWUmY29ubmVjdFRpbWVvdXQ9NQ==", Base64.DEFAULT);
             URL = new String(data, "UTF-8");
 
-            data = Base64.decode("dTgwOTIzMDcwOF9zaGl6X2xpdmU=", Base64.DEFAULT);
-            USERNAME = new String(data, "UTF-8");
-
-            data = Base64.decode("QEJjMTIzMTE=", Base64.DEFAULT);
-            PASSWORD = new String(data, "UTF-8");
+//            data = Base64.decode("dTgwOTIzMDcwOF9zaGl6X2xpdmU=", Base64.DEFAULT);
+//            USERNAME = new String(data, "UTF-8");
+//
+//            data = Base64.decode("QEJjMTIzMTE=", Base64.DEFAULT);
+//            PASSWORD = new String(data, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
     }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL);
+        //return DriverManager.getConnection("jdbc:mysql://151.106.117.51/u809230708_shiz_db?user=u809230708_shiz_live&password=@Bc12311&useUnicode=true&characterEncoding=UTF-8");
+        return DriverManager.getConnection("jdbc:mysql://192.168.1.4:3306/crewardtrade?useSSL=false&allowPublicKeyRetrieval=true&useUnicode=yes&characterEncoding=UTF-8&connectTimeout=5&user=steveng&password=abc12311");
+//        return DriverManager.getConnection(URL);
     }
 }
